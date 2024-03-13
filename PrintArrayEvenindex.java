269ba3978515c0e0bb4e57f131221a07994de036
Gokul Krish Nan <87110207+ImGokulKrishNan@users.noreply.github.com>
2024-03-13 19:21:07 +0530

    ArrayEvenindex.java
    Print Array Even Index

---
+++ PrintArrayEvenindex.java
@@ -0,0 +1,16 @@
+public class EvenArray {
+    public static void main(String[] args) {
+        int[] arr={14,48,65,47,77};
+                
+
+                for(int i=0;i<arr.length;i++){
+
+                    if(i%2==0){
+
+                        System.out.println(i+"  Even index   "+arr[i]);
+
+                    }
+                }
+    }
+    
+}
