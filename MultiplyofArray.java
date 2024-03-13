db597a55972c244021e78621640a079325e72e6b
Gokul Krish Nan <87110207+ImGokulKrishNan@users.noreply.github.com>
2024-03-13 19:37:46 +0530

    Create MultiplyofArray.java

---
+++ MultiplyofArray.java
@@ -0,0 +1,14 @@
+public class MultiplyofArray {
+    public static void main(String[] args) {
+             
+        int arr[] = { 12, 3, 4, 15 };;
+        int a=1;
+
+        for(int i=0;i<arr.length;i++){
+   
+           a=a*arr[i];
+
+        }System.out.println(a);
+    }
+    
+}
