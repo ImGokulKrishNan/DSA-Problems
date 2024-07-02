6f3f7326fda461a33b2d8c0caba7ce1882befd35
gokul <gokulpv532@gmail.com>
2024-07-02 18:21:34 +0530

    modify

--- Frequencyarray.java
+++ Frequencyarray.java
@@ -1,9 +1,9 @@
 /*
 find the frequency of each element in the array or Counting frequencies of array elements
-*/
+
 
 import java.util.*;
-public class Test {
+class Test {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 1, 6, 1, 2, 5, 6, 7, 8};
         //{1=3, 2=2, 3=1, 4=1, 5=1, 6=2, 7=1, 8=1}
@@ -37,3 +37,4 @@ public class Test {
     }
     }
 ///{1=3, 2=2, 3=1, 4=1, 5=1, 6=2, 7=1, 8=1}
+*/
