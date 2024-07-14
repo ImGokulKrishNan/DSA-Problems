2152b797c80ccc8d5a78ab4bcc07257b905067f9
Gokul Krish Nan <87110207+ImGokulKrishNan@users.noreply.github.com>
2024-07-14 22:50:20 +0530

    Update Pattern_Printing.java

--- Pattern_Printing.java
+++ Pattern_Printing.java
@@ -1,7 +1,5 @@
 Given a number **N**. The task is to print a series of asterisk(*****) from **1 till N** terms with increasing order and difference being 1.
-
 **Example 1:**
-
 **Input:**
 N = 3
 **Output:**
@@ -10,7 +8,6 @@ N = 3
 First, print 1 asterisk then space after
 that print 2 asterisk and space after that 
 print 3 asterisk now stop as N is 3.
-
 **Example 2:**
 
 **Input:**
