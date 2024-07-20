0c5162e99f81765dcded0aca6f36b34d4d4dd988
Gokul Krish Nan <87110207+ImGokulKrishNan@users.noreply.github.com>
2024-07-20 23:36:03 +0530

    Update RemoveduplicateSortedArray.java

--- RemoveduplicateSortedArray.java
+++ RemoveduplicateSortedArray.java
@@ -1,11 +1,15 @@
 import java.util.Arrays;
+
 public class RemoveduplicateSortedArray {
+    
     public static void main(String[] args) {
+       
         int[] n = {1,2,2};
 
        int k= remove(n);
         for (int i = 0; i < k ; i++) {
             System.out.print(n[i]);
+            
         }
     }
     static int remove(int[] n){
