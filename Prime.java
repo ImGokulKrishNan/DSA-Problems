4837119f9e55cdecb5297effc6237f93e291e5c7
Gokul Krish Nan <87110207+ImGokulKrishNan@users.noreply.github.com>
2024-07-29 23:52:42 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,23 +1,25 @@
-package Maths;
-
 public class Prime {
     public static void main(String args[]) {
+        int n=10;
+        System.out.println(countPrimes(n));
 
-        for (int i = 0; i < 100; i++) {
+    }
+    static  int countPrimes ( int n){
+        if (n <= 1)
+            return 0;
+        int count = 0;
+        for (int i = 2; i < n; i++) {
             if (pri(i)) {
-                System.out.print(i+" ");
+                 count++;
             }
         }
+        return count;
     }
-    static boolean pri(int n){
-
-        if(n<=1)
-            return false;
-        for (int i = 2; i < Math.sqrt(n) ; i++) {
-            if(n%i==0)
+    static boolean pri ( int n){
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
                 return false;
+            }
         }
-
         return true;
     }
-}
